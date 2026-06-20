e1e417b58fdb43b917e685cf3f563e223b075343
unknown <Administrator@Taj-SulemanAyaz-HO.TAJ.COM>
2026-06-20 12:27:25 +0500

    Sitemap BaseURL changed to coltechs.netlify.app

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://coltech.co";
+  const baseUrl = "https://coltechs.netlify.app";
 
   const routes = [
     "",
